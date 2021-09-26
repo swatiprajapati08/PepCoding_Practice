b70e4a6f27a7084ba2246f74b9ac85c489bc2211
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2021-09-26 16:24:35 +0530

    Create Queue To Stack Adapter - Pop Efficient.java

---
+++ Stack and Queue/Queue To Stack Adapter - Pop Efficient.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static class QueueToStackAdapter {
+    Queue<Integer> mainQ;
+
+
+    public QueueToStackAdapter() {
+      mainQ = new ArrayDeque<>();
+
+    }
+
+    int size() {
+      return mainQ.size();
+    }
+
+    void push(int val) {
+      Queue<Integer> helperQ = new ArrayDeque<>();
+      while (mainQ.size() > 0)
+        helperQ.add(mainQ.remove());
+
+      //add in main queue
+      mainQ.add(val);
+
+      //put helper in mainQ
+      while (helperQ.size() > 0)
+        mainQ.add(helperQ.remove());
+
+
+    }
+
+    int pop() {
+      if (mainQ.size() == 0)
+      {
+        System.out.println("Stack underflow");
+        return -1;
+      }
+      return mainQ.remove();
+    }
+
+    int top() {
+      if (mainQ.size() == 0)
+      {
+        System.out.println("Stack underflow");
+        return -1;
+      }
+      return mainQ.peek();
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    QueueToStackAdapter st = new QueueToStackAdapter();
+
+    String str = br.readLine();
+    while (str.equals("quit") == false) {
+      if (str.startsWith("push")) {
+        int val = Integer.parseInt(str.split(" ")[1]);
+        st.push(val);
+      } else if (str.startsWith("pop")) {
+        int val = st.pop();
+        if (val != -1) {
+          System.out.println(val);
+        }
+      } else if (str.startsWith("top")) {
+        int val = st.top();
+        if (val != -1) {
+          System.out.println(val);
+        }
+      } else if (str.startsWith("size")) {
+        System.out.println(st.size());
+      }
+      str = br.readLine();
+    }
+  }
+}
